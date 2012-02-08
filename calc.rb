0dd326e328b1d3ac20e7fb517f0da69e5e57611b
Vinicius Lira <onilira@gmail.com>
2012-02-08 00:09:17 -0300

    first lessons

---
+++ calc.rb
@@ -0,0 +1,18 @@
+﻿#econding: utf-8
+
+def sumario &bloco
+	inicio = Time.now
+	bloco.call
+	tempo = Time.now - inicio
+	puts 'Duracaoo do programa: '+ tempo.to_s
+end
+
+sumario do
+	numero = 1
+	
+	1000000.times do
+		numero = numero + numero
+	end
+	
+puts numero.to_s.length.to_s
+end
---
+++ montyhall2.rb
@@ -0,0 +1,30 @@
+#econding: utf-8
+100.times do
+	hits = 0;
+	10000.times do
+		flag = ''
+		
+		#Setting the player's choice and the right choice randomly
+		right_choice =  rand(3)+1
+		first_choice = rand(3)+1
+		
+		#Setting the door that will be opened by the host. Remember it must be different from the door choosen by the player and from the winning door (the first two values selected before).
+		door_excluded = rand(3)+1
+		while (door_excluded == right_choice or door_excluded == first_choice)
+			door_excluded = rand(3)+1
+		end
+		#Now, it sets the second choice, assuming that the player will always change his initial choice.
+		second_choice = rand(3)+1
+		while (second_choice == door_excluded or second_choice == first_choice)
+			second_choice = rand(3)+1
+		end
+		#Here it increases the number of hit in case the second choice is right.
+		if (second_choice == right_choice)
+			hits+=1
+			flag = 'HIT!'
+		end
+		#puts 'First choice: '+first_choice.to_s+' Second choice: '+second_choice.to_s+' Right choice: '+right_choice.to_s+' '+flag
+	end
+
+	puts 'Total hits: '+hits.to_s
+end
